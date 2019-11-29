90bcfea4318c7e8551bea84441c133dd887f4ad8
Adam <adam.goncalves@epitech.eu>
2019-11-29 16:39:47 +0100

    role fix

--- src/roles.js
+++ src/roles.js
@@ -8,9 +8,9 @@ const localRoles = [
     {name:"GRAPHISTE", emoji: ":art:"},
     {name:"MANAGER", emoji: ":briefcase:"},
     {name:"MONTEUR VIDEO", emoji: ":movie_camera:"},
-    {name:"RESP.EVENEMENTIEL", emoji: ":tada:"},
-    {name:"RESP.MARKETING", emoji: ":chart_with_upwards_trend:"},
-    {name:"RESP.COMMUNICATION", emoji: ":globe_with_meridians:"},
+    {name:"RESP. EVENEMENTIEL", emoji: ":tada:"},
+    {name:"RESP. MARKETING", emoji: ":chart_with_upwards_trend:"},
+    {name:"RESP. COMMUNICATION", emoji: ":globe_with_meridians:"},
     {name:"MODERATEUR", emoji: ":detective:"},
     {name:"FONDATEUR", emoji: ":rocket:"}
 ]
